74466c7b989bde21639f7d5376e193fa7b5569ad
Ernestas <39731706+ErnestasD@users.noreply.github.com>
2018-08-08 14:12:42 +0300

    Update ArticleController.php

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -118,3 +118,21 @@
       $response = new Response();
       $response->send();
     }
+    
+    // /**
+    //  * @Route("/article/save")
+    //  */
+    // public function save() {
+    //   $entityManager = $this->getDoctrine()->getManager();
+    
+    //   $article = new Article();
+    //   $article->setTitle('Article Two');
+    //   $article->setBody('This is the body for article two');
+    
+    //   $entityManager->persist($article);
+    
+    //   $entityManager->flush();
+    
+    //   return new Response('Saved an article with the id of  '.$article->getId());
+    // }
+  }
