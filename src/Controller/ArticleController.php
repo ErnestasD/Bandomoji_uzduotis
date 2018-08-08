82df643b07bd36853137ac65506bc1621af9f2ef
Ernestas <39731706+ErnestasD@users.noreply.github.com>
2018-08-08 14:11:17 +0300

    Update ArticleController.php

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -103,3 +103,18 @@
       $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
       return $this->render('articles/show.html.twig', array('article' => $article));
     }
+    
+    /**
+     * @Route("/article/delete/{id}")
+     * @Method({"DELETE"})
+     */
+    public function delete(Request $request, $id) {
+      $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+      
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->remove($article);
+      $entityManager->flush();
+      
+      $response = new Response();
+      $response->send();
+    }
