886d3330753107f69619d75c9ab4fb47b4c80b64
Ernestas <39731706+ErnestasD@users.noreply.github.com>
2018-08-08 14:10:10 +0300

    Update ArticleController.php

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -95,3 +95,11 @@
         'form' => $form->createView()
       ));
     }
+    
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show($id) {
+      $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+      return $this->render('articles/show.html.twig', array('article' => $article));
+    }
